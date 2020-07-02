378b60c35094ba7aa4c1cbb53fe76c9a5af79acc
Rahul Holani <65071340+RahulHolani1008@users.noreply.github.com>
2020-07-02 19:58:03 +0530

    test

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -12,7 +12,7 @@
               </svg>
             </span>
             <span class="menu_name">Statistics</span>
-            <i class="fas fa-angle-right"></i> Shrikant
+            <i class="fas fa-angle-right"></i> Sushmita
           </a>
           <div class="sub-menu">
             <ul class="list-unstyled">
