7323b78f1283403ba6cab1af9b5085cbaaf1ed67
Rahul Holani <65071340+RahulHolani1008@users.noreply.github.com>
2020-07-02 19:42:35 +0530

    test1

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -12,7 +12,7 @@
               </svg>
             </span>
             <span class="menu_name">Statistics</span>
-            <i class="fas fa-angle-right"></i> hehehehe
+            <i class="fas fa-angle-right"></i> hehehehedfdsf
           </a>
           <div class="sub-menu">
             <ul class="list-unstyled">
